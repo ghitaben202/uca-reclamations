5405a4339ffd14c3100421bd1fdc39dfa4cf1555
Sanae 202 <sanae.202@gmail.com>
2025-03-18 11:59:07 +0000

    commit 6 - modification de style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" style="background-color:rgba(229, 221, 208, 0.5);">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid text-center" style="background-color:rgba(229, 221, 208, 0.5);height:600px">
-    <header>
-        <img src="{{ asset('images/logo.jpeg') }}" alt="Logo" class="mb-4 d-flex mx-auto d-block" width="100">
+<div class="container-fluid text-center py-5">
+    <header class="mb-4">
+        <img src="{{ asset('images/logo.jpeg') }}" alt="Logo" class="d-flex mx-auto d-block" width="100">
     </header>
-
-    <div class="row justify-content-center" >
-        <h2 class="text-black mb-4 ">Bienvenue dans le porteil U-Réclamations de l'UCA</h2>
-        <div class="col-md-5">
-            <div class="p-4" style=" background-color: rgba(213, 200, 145, 0.5);">
+    <h2 class="text-black mb-4 ">Bienvenue dans le porteil U-Réclamations de l'UCA</h2>
+    <div class="row justify-content-center gy-4" >
+        <!-- Connexion -->
+        <div class="col-lg-5 col-md-6 col-10">
+            <div class="p-4 rounded shadow" style=" background-color: rgba(213, 200, 145, 0.5);">
                 <h4 class="text-center mb-3 fw-bold ">Connexion</h4>
                 <form action="{{ route('login') }}" method="POST">
                     @csrf
-                    <div class="mb-3">
+                    <div class="mb-3 text-start">
                         <label class="form-label ">Email</label>
                         <input type="email" class="form-control" name="email" required>
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 text-start">
                         <label class="form-label ">Mot de passe</label>
                         <input type="password" class="form-control" name="password" required>
                     </div>
@@ -26,9 +26,12 @@
                 </form>
             </div>
         </div>
-        <div class="col-md-5" style=" background-color: rgba(213, 200, 145, 0.5);">
-            <h4 class="text-center mb-4 fw-bold "  style="margin-top:100px">Pas d’email académique ? Inscrivez-vous ici !</h4>
-            <a href="{{ route('register') }}" class="btn btn-warning w-100 ">S'inscrire</a>
+        <!-- Inscription -->
+        <div class="col-lg-5 col-md-6 col-10">
+            <div class="p-4 rounded shadow h-100 d-flex flex-column justify-content-center" style="background-color: rgba(213, 200, 145, 0.5);">
+                <h4 class="text-center mb-4 fw-bold "  style="margin-top:100px">Pas d’email académique ? Inscrivez-vous ici !</h4>
+                <a href="{{ route('register') }}" class="btn btn-warning w-100 ">S'inscrire</a>
+            </div>
         </div>
     </div>
 </div>
