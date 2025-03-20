0217faf1c6ddcd51e14216cac85adfa9a25d634d
Ghita <benachir.g@gmail.com>
2025-03-20 10:13:54 +0000

    Fix: Normalize line endings to LF

---
+++ database/migrations/2025_03_20_100514_create_reclamations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reclamations', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('titre');
+            $table->date('date_creation');
+            $table->date('date_update');
+            $table->string('statut')->default('en cours');
+            $table->unsignedBigInteger('utilisateur_id')->nullable(); 
+            $table->unsignedBigInteger('type_reclamations_id')->nullable(); 
+            $table->foreign('utilisateur_id')->references('id')->on('utilisateurs')->onDelete('set null');
+            $table->foreign('type_reclamations_id')->references('id')->on('type_reclamations')->onDelete('set null');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reclamations');
+    }
+};
