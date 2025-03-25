d27bde8434be204a119c8aefc9c54d35eafabbbd
Ghita <benachir.g@gmail.com>
2025-03-25 14:23:28 +0000

    commit 14 - tableau des reclamations / datatable

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -43,6 +43,24 @@ public function showReclamations()
     return view('reclamations.index', compact('reclamations'));
 }
 
+
+public function getData(Request $request)
+{
+    $query = Reclamation::select(['id', 'titre', 'statut', 'date_creation']);
+
+    return DataTables::of($query)
+        ->editColumn('date_creation', function ($reclamation) {
+            return $reclamation->date_creation->format('d/m/Y H:i');
+        })
+        ->addColumn('actions', function ($reclamation) {
+            return '<a href="#" class="btn btn-success btn-sm"><i class="fas fa-search"></i></a>
+                    <a href="#" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
+                    <a href="#" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>';
+        })
+        ->rawColumns(['actions'])
+        ->make(true);
+}
+
 }
 
 
