f7e8014ad8b318a058dd94ce74944f9f5615d663
Ghita <benachir.g@gmail.com>
2025-03-26 12:16:50 +0000

    commit 15 - tableau des reclamations finie

--- resources/views/reclamations/index.blade.php
+++ resources/views/reclamations/index.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -97,6 +97,7 @@
 
     <div class="container">
         <h4>Mes Réclamations</h4>
+        <hr>
         <table id="reclamationsTable" class="table table-bordered">
             <thead>
                 <tr>
@@ -139,25 +140,38 @@
             }
         });
 
-
-  
-    $(document).ready(function() {
-    $('#reclamationsTable').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('reclamations.data') }}",
-        columns: [
-            { data: 'id', name: 'id' },
-            { data: 'titre', name: 'titre' },
-            { data: 'date_creation', name: 'date_creation' },
-            { data: 'statut', name: 'statut' },
-            { data: 'actions', name: 'actions', orderable: false, searchable: false }
-        ],
-        language: {
-            url: "{ asset('assets/i18n/French.json') }}"
+        $(document).ready(function() {
+        $('#reclamationsTable').DataTable({
+        "paging": true,      
+        "searching": true,   
+        "ordering": true,    
+        "info": true,        
+        "lengthMenu": [5, 10, 25, 50], 
+        "language": {
+            "sProcessing":     "Traitement en cours...",
+            "sSearch":         "Rechercher :",
+            "sLengthMenu":     "Afficher _MENU_ éléments",
+            "sInfo":           "Affichage de _START_ à _END_ sur _TOTAL_ éléments",
+            "sInfoEmpty":      "Affichage de 0 à 0 sur 0 éléments",
+            "sInfoFiltered":   "(filtré à partir de _MAX_ éléments au total)",
+            "sInfoPostFix":    "",
+            "sLoadingRecords": "Chargement en cours...",
+            "sZeroRecords":    "Aucun élément à afficher",
+            "sEmptyTable":     "Aucune donnée disponible dans le tableau",
+            "oPaginate": {
+                "sFirst":    "Premier",
+                "sPrevious": "Précédent",
+                "sNext":     "Suivant",
+                "sLast":     "Dernier"
+            },
+            "oAria": {
+                "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
+                "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
+            }
         }
     });
-    });
+});
+
 </script>
     </script>
 </body>
