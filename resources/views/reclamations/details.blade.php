ecdd829e8c2f53d59d060b4847e9142761405c57
Ghita <benachir.g@gmail.com>
2025-03-27 11:04:43 +0000

    commit 17 - details reclamations en cours

--- resources/views/reclamations/details.blade.php
+++ resources/views/reclamations/details.blade.php
@@ -4,10 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -54,14 +50,16 @@
             color:black;
         }
         #toggleSidebar{
-         background-color:rgba(234, 213, 192, 0.4);
+           background-color:rgba(234, 213, 192, 0.4);
         }
-        p{
-            color:rgba(156, 109, 50, 0.83);
+        .user{
+            
         }
     </style>
 </head>
 <body>
+
+    <!--NAVBAR-->
     <nav class="navbar navbar-expand-lg navbar-light" id="navb">
         <div class="container-fluid">
             <button class="btn" id="toggleSidebar">☰</button>
@@ -74,7 +72,7 @@
             @endif
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         Déconnexion</a>
                     </li>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -84,48 +82,51 @@
             </div>
         </div>
     </nav>
+
+    <!--SIDEBAR-->
     <div id="sidebar" class=" text-dark p-3">
     @if(auth()->check())
-        <p class="text-center">{{ auth()->user()->nom }}</p>
+        <p class="text-center user">{{ auth()->user()->nom }}</p>
     @endif
         <ul class="nav flex-column">
-            <li class="nav-item" id="list1"><a href="{{ route('dashboard') }}" class="nav-link text-dark">Accueil</a></li>
-            <li class="nav-item" id="list2"><a href="#" class="nav-link text-dark">Mes Reclamations</a></li>
+            <li class="nav-item" id="list1"><a href="#" class="nav-link text-dark">Accueil</a></li>
+            <li class="nav-item" id="list2"><a href="{{ route('reclamations.index') }}" class="nav-link text-dark">Mes Reclamations</a></li>
         </ul>
     </div>
+
+
+    <!--CONTENT-->
     <div id="content">
+        <div class="container">
+            <h4>Détails de la Réclamation</h4>
+            <hr>
+            <div class="row px-2">
 
-    <div class="container">
-        <h4>Mes Réclamations</h4>
-        <hr>
-        <table id="reclamationsTable" class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Objet</th>
-                    <th scope="col">Statut</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($reclamations as $reclamation)
-                    <tr>
-                        <td>{{ $reclamation->id }}</td>
-                        <td>{{ $reclamation->titre }}</td>
-                        <td>{{ $reclamation->statut }}</td>
-                        <td>{{ $reclamation->date_creation }}</td>
-                        <td>
-                        <a href="{{ route('reclamations.details', $reclamation->id) }}" class="btn btn-info">Voir les détails</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                <div class="col-md-8 ">
+                    <div class="details bg-light p-3">
+                    <h4 class="">Titre :<span style="font-size:14px; margin-left:10px; color: #6f6d72;">{{ $reclamation->titre }}</span></h4>
+                    <hr>
+                    <h4 class="">Description :</h4><hr>
+                    <span style="font-size:14px; margin-left:10px; color: #6f6d72;">{{ $reclamation->description }}</span>
+                    
+                    </div>
+                </div>
 
-
-
+                <div class="col-md-4">
+                    <div class="infos bg-light p-3">
+                    <p class="">Statut:<span style="font-size:14px; margin-left:10px; padding:5px 5px; color: #6f6d72;">{{ $reclamation->titre }}</span></p>
+                    <hr>
+                    <p class="">Date de soumission:
+                    <span style="font-size:14px; margin-left:10px; color: #6f6d72;">{{ $reclamation->date_creation}}</span>
+                    </p>
+                    <hr>
+                    <p class="">Catégorie:<span style="font-size:14px; margin-left:10px; padding:5px 5px; color: #6f6d72;">{{ $reclamation->typeReclamation->nom ?? 'Non spécifié' }}
+                    </span></p>
+                    <hr>
+                </div>
+            </div>
+        
+        </div>
     </div>
     <script>
         document.getElementById('toggleSidebar').addEventListener('click', function() {
@@ -139,40 +140,6 @@
                 content.style.marginLeft = '0';
             }
         });
-
-        $(document).ready(function() {
-        $('#reclamationsTable').DataTable({
-        "paging": true,      
-        "searching": true,   
-        "ordering": true,    
-        "info": true,        
-        "lengthMenu": [5, 10, 25, 50], 
-        "language": {
-            "sProcessing":     "Traitement en cours...",
-            "sSearch":         "Rechercher :",
-            "sLengthMenu":     "Afficher _MENU_ éléments",
-            "sInfo":           "Affichage de _START_ à _END_ sur _TOTAL_ éléments",
-            "sInfoEmpty":      "Affichage de 0 à 0 sur 0 éléments",
-            "sInfoFiltered":   "(filtré à partir de _MAX_ éléments au total)",
-            "sInfoPostFix":    "",
-            "sLoadingRecords": "Chargement en cours...",
-            "sZeroRecords":    "Aucun élément à afficher",
-            "sEmptyTable":     "Aucune donnée disponible dans le tableau",
-            "oPaginate": {
-                "sFirst":    "Premier",
-                "sPrevious": "Précédent",
-                "sNext":     "Suivant",
-                "sLast":     "Dernier"
-            },
-            "oAria": {
-                "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
-                "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
-            }
-        }
-    });
-});
-
-</script>
     </script>
 </body>
 </html>
